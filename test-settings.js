fef3b6f15b17fe4ad884867d9299e5419b3dec6b
hakan tektaş <hakan.tektas@mobven.com>
2022-08-09 17:38:37 +0300

    gw update
    gw update

--- test-settings.js
+++ test-settings.js
@@ -4,7 +4,7 @@ var DATA = {
        "momentum.token": "<momentum-suite-access-key>",
        "momentum.app": "ms://<hashed-app-id>", 
        "momentum.hostname": "console.momentumsuite.com",
-       "momentum.gw": 4033,
+       "momentum.gw": 443,
        "momentum.path": "/gateway/wd/hub/",
        "momentum.protocol": "https",
        "momentum.deviceList": [4033] //Comma seperated device list for parallel-test run
@@ -19,5 +19,4 @@ var DATA = {
        "deviceName": "emulator-5554"
        }
    };
-   
    exports.DATA = DATA;
